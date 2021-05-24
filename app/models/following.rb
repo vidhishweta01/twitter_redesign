6565e58026036f372f6cd8b7d85d921a0481872e
vidhishweta01 <vidhishweta01@gmail.com>
2021-05-24 13:54:23 +0530

    removed name error

--- app/models/following.rb
+++ app/models/following.rb
@@ -3,10 +3,10 @@ class Following < ApplicationRecord
   belongs_to :followed, class_name: 'User'
 
   def self.find_following(id1, id2)
-    if Following.where(user_id: id1, Followed_id: id2).empty?
-      Following.where(user_id: id2, Followed_id: id1)[0].id
+    if Following.where(user_id: id1, followed_id: id2).empty?
+      Following.where(user_id: id2, followed_id: id1)[0].id
     else
-      Following.where(user_id: id1, Followed_id: id2)[0].id
+      Following.where(user_id: id1, followed_id: id2)[0].id
     end
   end
 end
