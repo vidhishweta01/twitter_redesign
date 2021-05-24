68955d7cb12d2b65356959e93420285c4511d9ab
vidhishweta01 <vidhishweta01@gmail.com>
2021-05-24 11:14:19 +0530

    added screenshot added readme

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -2,7 +2,7 @@ module UserHelper
   # rubocop:disable Metrics/PerceivedComplexity
   # rubocop:disable Metrics/CyclomaticComplexity
   def follow(user) 
-    if signed_in? && !current_user.already_followed?(current_user.id, user.id) && currdist_user != user
+    if signed_in? && !current_user.already_followed?(current_user.id, user.id) && current_user != user
       link_to 'Follow', followings_create_path(ids: { id1: current_user.id, id2: user.id })
     elsif signed_in? && current_user.already_followed?(current_user.id, user.id) && current_user != user
       link_to 'Unfollow', followings_destroy_path(following_id: Following.find_following(current_user.id, user.id))
