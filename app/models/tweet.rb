a8b39ab6b176e3f4d08247e43b8291c7811588b9
vidhishweta01 <vidhishweta01@gmail.com>
2021-05-26 15:06:15 +0530

    removed linter errors

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,8 +1,7 @@
 class Tweet < ApplicationRecord
-  validates :content, presence: true, length: { maximum: 1000,
-              too_long: '1000 characters in post is the maximum allowed.' } # rubocop:disable Layout/HashAlignment
+  validates :content, presence: true, length: { maximum: 300,
+              too_long: '300 characters in tweet is the maximum allowed.' } # rubocop:disable Layout/HashAlignment
   validates :user_id, presence: true
   belongs_to :user
-
   scope :ordered_by_most_recent, -> { order(created_at: :desc) }
 end
